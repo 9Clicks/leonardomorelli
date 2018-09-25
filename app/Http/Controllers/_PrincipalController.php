b960d6d8e2ceeabab3f9adbba9dbc725ec9ff26b
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-25 11:41:43 -0300

    Controler/View/Rota Configurado.

--- app/Http/Controllers/_PrincipalController.php
+++ app/Http/Controllers/_PrincipalController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
+use App\appSettings;
 use Illuminate\Http\Request;
 
 class _PrincipalController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('principal.index');
+    }
 }

5569cd09b155d3226875eb37686cad19015f4772
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-25 11:35:03 -0300

    Head/Html adicionado.

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -1,7 +1,39 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: noveclicks
- * Date: 25/09/2018
- * Time: 11:30
- */
+<!doctype html>
+
+<html lang="">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+    <link rel="icon" type="image/png" href="" />
+
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="Author" content="">
+    <meta name="language" content="" />
+    <meta name="URL" content="http://" />
+    <meta name="subject" content="" />
+    <meta name="rating" content="general" />
+    <meta name="updated" content="daily" />
+    <meta name="robots" content="index, follow" />
+    <meta name="audience" content="all" />
+    <meta name="Publisher" content="" />
+    <meta name="ia_archiver" content="index, follow" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="msnbot" content="index, follow" />
+    <meta name="Search Engines" content="" />
+    <meta name="audience" content="all" />
+    <meta name="revisit-after" content="1 days" />
+    <meta name="generator" content="">
+    <meta name="document-classification" content="" />
+
+</head>
+
+<body>
+
+</body>
+
+</html>
